6f56724abb0bcae73adad76133ff6e72a4ebdd35
Peter Tucker <ptucker@whitworth.edu>
2018-06-09 16:42:42 -0700

    Added feedback

--- 11.13/Course.cpp
+++ 11.13/Course.cpp
@@ -3,6 +3,8 @@
 #include "Course.h" 
 using namespace std; 
 
+//PT -- 11/20
+
 Course::Course(const string& courseName, int capacity) {
  numberOfStudents = 0;
 	 this->courseName = courseName;
@@ -16,6 +18,14 @@ Course::Course(const Course & course) {
 	numberOfStudents = course.numberOfStudents();
 	capacity = course.capacity();
 	students = new string[capacity];
+	
+	//PT -- need to copy the students from course to this class
+	//  -2
+	/*
+	for (int i=0; i<course.numberOfStudents; i++) {
+		students[i] = course.students[i];
+	}
+	*/
 }
 
 
@@ -34,6 +44,8 @@ Course::Course(const Course & course) {
  students[numberOfStudents] = name;
 	 numberOfStudents++;
 
+	 //PT -- don't need to allocate a new array unless we're passed capacity
+	 //  -1
 	 string *x = new string[numberOfStudents];
 	 
 	 // Makes larger array if the capacity is exceeded. 
@@ -42,7 +54,13 @@ Course::Course(const Course & course) {
 		{
 			x[i] = students[i];
 		}
+		 //PT -- Once this is done, delete the old students pointer and 
+		 //      set students = x so that you are hanging onto the new data
+		 // delete[] students;
+		 // students = x;
+		 // -2
 	}
+	 //PT -- don't need this else statement
 	 else
 	 {
 		 students[numberOfStudents];
@@ -51,6 +69,20 @@ Course::Course(const Course & course) {
  }
 
  void Course::dropStudent(const string& name)  {
+	 //PT -- search through the array and find the name, and then move 
+	 //      all the other students back one slot in the array
+	 // -3
+	 /*
+	 int i=0;
+	 while (i < numberOfStudents && students[i] != name)
+	 	i++;
+	 //Found the student. Move everyone else back one slot
+	 while (i < numberOfStudents - 1) {
+	 	students[i] = students[i+1];
+		i++;
+	 }
+	 numberOfStudents--;
+	 */
 	 
 	 string array[10];
 	 array[10]= name;
@@ -59,7 +91,16 @@ Course::Course(const Course & course) {
 
 		 array[i] = "";
 		 }
+//PT -- you've lost a closing } for this function
 
+//PT -- clear simply removes all students, so it doesn't take a parameter
+// -3
+/*
+void Course::clear() {
+	//really, we only need to set numberOfStudent to 0. That means we have no students
+	numberOfStudents = 0;
+}
+*/
  void Course::clear(const string & name) {
 	
 	 students[100] = name;
